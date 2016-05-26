86768d83067745def16761db24ce496e3125399e
Joel Barnes <oelarnes@gmail.com>
2016-05-25 22:36:23 -0400

    test script works

--- mtgreatest/process_results/test_script.py
+++ mtgreatest/process_results/test_script.py
@@ -8,15 +8,15 @@ cursor = Cursor()
 event_info = cursor.execute('select event_id, event_link from event_table where results_loaded = 1')
 event_info = [dict(zip(('event_id','event_link'), item)) for item in event_info] #this should be a method (or default return structure) in rdb
 
-soups = [scrape_results.event_soup(row['event_link']) for row in event_info]
-
 failed = []
 
-for i in range(len(soups)):
+for row in event_info:
+    soup = scrape_results.event_soup(row['event_link'])
+    print 'scraping standings for event {}'.format(row['event_id'])
     try:
-        scrape_results.scrape_standings(soups[i], event_info[i]['event_id'])
+        scrape_results.scrape_standings(soup, row['event_id'])
     except:
-        failed.append(event_info[i]['event_id'])
+        failed.append(row['event_id'])
 
 
 
