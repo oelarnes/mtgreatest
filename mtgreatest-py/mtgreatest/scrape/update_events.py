f5c0a3bcf536141946c3fd19ab04ce71061aa192
oelarnes <oelarnes@gmail.com>
2016-07-09 20:25:14 +0000

    need to make rdb results keyed tables

--- mtgreatest-py/mtgreatest/scrape/update_events.py
+++ mtgreatest-py/mtgreatest/scrape/update_events.py
@@ -54,6 +54,8 @@ def update_event(event_info):
     if len(result) == 0:
         event_info['process_status'] = 0
         cursor.insert('event_table', [event_info])
+    elif len(result) == 1:
+        #results should be tables of dicts
     cursor.close()
     return
 
@@ -68,45 +70,54 @@ def update_events():
                 continue
             season = section.find('span').text.partition(' ')[0]
             print 'season {}'.format(season)
-            paragraphs =  section.find('div').find_all('p')
+            paragraphs =  section.find('div').find_all(['p', 'h4'])
             d = {'event_type' : 'Championship', 'season' : season}
             for paragraph in paragraphs:
-                for child in paragraph.children:
-                    #print child
-                    #print '/\/\/\/\/\/\/\/'
-                    if child.name in ['b', 'strong']:
-                        if 'Grand Prix' in child.text:
-                            d['event_type'] = 'Grand Prix'
-                        if 'Pro Tour' in child.text:
-                            d['event_type'] = 'Pro Tour'
-                        if 'Masters' in child.text:
-                            d['event_type'] = 'Masters'
-                    elif child.name in ['i','em']:
-                        if 'fmt_desc' in d:
-                            d['fmt_desc'] += child.text
-                    elif child.name == 'br':
-                        if 'event_link' in d:
-                            update_event(d)
-                            d = {'event_type' : d['event_type'], 'season' : season}
-                    elif child.name == 'a':
-                        if len(child.text) is 0:
-                            continue
-                        d['event_link'] = clean_magic_link(child['href'])
-                        d['event_id'] = event_id_from_link(d['event_link'])
-                        if d['event_type'] == 'Championship':
-                            d['event_full_name'] = child.text
-                        else:
-                            d['event_full_name'] = d['event_type'] + ' ' + child.text
-                    elif child.name is None:
-                        if 'day_1_date' not in d:
-                            d['day_1_date'] = info_text_to_date(child)
-                            if d['event_type'] in ['Grand Prix', 'Pro Tour'] and d['day_1_date'] is not None:
-                                d['day_2_date'] = d['day_1_date'] + timedelta(1)
-                            if d['event_type'] == 'Pro Tour' and d['day_1_date'] is not None:
-                                d['day_3_date'] = d['day_2_date'] + timedelta(1)
-                            d['fmt_desc'] = info_text_to_fmt_desc(child)
-                        elif 'fmt_desc' in d:
-                            d['fmt_desc'] += child
+                if paragraph.name == 'h4':
+                    if 'Grand Prix' in paragraph.text:
+                        d['event_type'] = 'Grand Prix'
+                    if 'Pro Tour' in paragraph.text:
+                        d['event_type'] = 'Pro Tour'
+                        print 'found PT'
+                    if 'Masters' in paragraph.text:
+                        d['event_type'] = 'Masters'
+                else:
+                    for child in paragraph.children:
+                        #print child
+                        #print '/\/\/\/\/\/\/\/'
+                        if child.name in ['b', 'strong']:
+                            if 'Grand Prix' in child.text:
+                                d['event_type'] = 'Grand Prix'
+                            if 'Pro Tour' in child.text:
+                                d['event_type'] = 'Pro Tour'
+                            if 'Masters' in child.text:
+                                d['event_type'] = 'Masters'
+                        elif child.name in ['i','em']:
+                            if 'fmt_desc' in d:
+                                d['fmt_desc'] += child.text
+                        elif child.name == 'br':
+                            if 'event_link' in d:
+                                update_event(d)
+                                d = {'event_type' : d['event_type'], 'season' : season}
+                        elif child.name == 'a':
+                            if len(child.text) is 0:
+                                continue
+                            d['event_link'] = clean_magic_link(child['href'])
+                            d['event_id'] = event_id_from_link(d['event_link'])
+                            if d['event_type'] == 'Championship':
+                                d['event_full_name'] = child.text
+                            else:
+                                d['event_full_name'] = d['event_type'] + ' ' + child.text
+                        elif child.name is None:
+                            if 'day_1_date' not in d:
+                                d['day_1_date'] = info_text_to_date(child)
+                                if d['event_type'] in ['Grand Prix', 'Pro Tour'] and d['day_1_date'] is not None:
+                                    d['day_2_date'] = d['day_1_date'] + timedelta(1)
+                                if d['event_type'] == 'Pro Tour' and d['day_1_date'] is not None:
+                                    d['day_3_date'] = d['day_2_date'] + timedelta(1)
+                                d['fmt_desc'] = info_text_to_fmt_desc(child)
+                            elif 'fmt_desc' in d:
+                                d['fmt_desc'] += child
                 if 'event_link' in d:
                     update_event(d)
                     d = {'event_type' : d['event_type'], 'season' : season}
